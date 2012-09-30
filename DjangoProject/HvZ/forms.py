4159dd9db5ca3df285c5ed8131b27774332e6dba
Jeff <jeff@babar.(none)>
2012-09-30 00:40:07 -0700

    Remove all tags from `forms.py`.

--- DjangoProject/HvZ/forms.py
+++ DjangoProject/HvZ/forms.py
@@ -63,12 +63,12 @@ class EatForm(forms.Form):
 
     feed_code = forms.CharField(label='Feed Code',
         max_length=5, min_length=5,
-        help_text="<div class='reminder'>Type in the 5 letter feed code of the person you ate</div>",
+        help_text="Type in the 5 letter feed code of the person you ate",
     )
     meal_day = forms.ChoiceField(
         choices=DAYS, initial=t.weekday(), required=True,
         widget=forms.RadioSelect,
-        help_text="<div class='reminder'>Select the day that you ate them.</div>",
+        help_text="Select the day that you ate them.",
     )
     meal_hour = forms.ChoiceField(label="Meal Time",
         choices=HOURS, initial=cur_hour, required=True,
@@ -77,15 +77,15 @@ class EatForm(forms.Form):
         choices=MINS, initial=cur_min, required=True)
     meal_ap = forms.ChoiceField(label="",
         choices=AP, initial=cur_ap, required=True,
-        help_text="<div class='reminder'>Select the time of the meal; if you can't remember just type in now.</div>",
+        help_text="Select the time of the meal; if you can't remember just type in now.",
     )
     location = forms.ModelChoiceField(queryset=Building.objects.filter(building_type='D').order_by('name'), required=False, empty_label="Don't Remember",
-        help_text="<div class='reminder'>Select the name of the closest building or landmark to the meal</div>",
+        help_text="Select the name of the closest building or landmark to the meal",
     )
     description = forms.CharField(label='Description',
         required=False,
         widget=forms.Textarea,
-        help_text="<div class='reminder'>If you got a particularly epic meal, go ahead and describe it here</div>",
+        help_text="If you got a particularly epic meal, go ahead and describe it here",
     )
 
 
@@ -93,61 +93,61 @@ class RegForm(forms.Form):
     first = forms.CharField(label='First Name',
         required=True,
         max_length=30,
-        help_text="<div class='reminder'>Your First Name</div>",
+        help_text="Your First Name",
     )
     last = forms.CharField(label='Last Name',
         required=True,
         max_length=30,
-        help_text="<div class='reminder'>Your Last Name</div>",
+        help_text="Your Last Name",
     )
     email = forms.EmailField(label='Email Address',
         required=True,
-        help_text="<div class='reminder'>Enter in an email address that you check regularly since you will be receiving game updates approximately 3 times per day during the week of the game. This email address will also be used to log you in to the website</div>",
+        help_text="Enter in an email address that you check regularly since you will be receiving game updates approximately 3 times per day during the week of the game. This email address will also be used to log you in to the website",
     )
     password = forms.CharField(required=True,
         widget=forms.PasswordInput,
-        help_text="<div class='reminder'>Enter a password that will be easy for you to remember but difficult for others to guess.</div>",
+        help_text="Enter a password that will be easy for you to remember but difficult for others to guess.",
     )
     school = forms.ModelChoiceField(queryset=School.objects.all().order_by('name'),
         required=True,
         empty_label="None",
-        help_text="<div class='reminder'>Select which school you attend. If you do not attend one of the 7Cs, select \"None\".</div>",
+        help_text="Select which school you attend. If you do not attend one of the 7Cs, select \"None\".",
     )
     dorm = forms.ModelChoiceField(queryset=Building.objects.filter(building_type='D').order_by('name'),
         required=True,
         empty_label="Off Campus",
-        help_text="<div class='reminder'>Select which dorm you expect to sleep in on most nights during the game. If you do not live on campus, select \"Off Campus\"</div>",
+        help_text="Select which dorm you expect to sleep in on most nights during the game. If you do not live on campus, select \"Off Campus\"",
     )
-    grad = forms.ChoiceField(label='Graduation Year', choices=(("2011", "2011"), ("2012", "2012"), ("2013", "2013"), ("2014", "2014"), ("2015", "2015"), ("2016", "2016"), ("2017", "2017"), ("", "Not a Student")), initial="2011", required=False, help_text="<div class='reminder'>Select which year you expect to graduate. If you don't know yet, select 5 years after the fall of your first year.</div>")
-    cell = USPhoneNumberField(required=False, help_text="<div class='reminder'>If you want to be able to text message the game's website enter in your phone number here. We will not use this number except in emergencies or in response to texts from you.</div>")
+    grad = forms.ChoiceField(label='Graduation Year', choices=(("2011", "2011"), ("2012", "2012"), ("2013", "2013"), ("2014", "2014"), ("2015", "2015"), ("2016", "2016"), ("2017", "2017"), ("", "Not a Student")), initial="2011", required=False, help_text="Select which year you expect to graduate. If you don't know yet, select 5 years after the fall of your first year.")
+    cell = USPhoneNumberField(required=False, help_text="If you want to be able to text message the game's website enter in your phone number here. We will not use this number except in emergencies or in response to texts from you.")
     oz = forms.BooleanField(label='OZ Pool',
         required=False,
-        help_text="<div class='reminder'>Check this box if you would like to begin afflicted with the zombie curse.</div>",
+        help_text="Check this box if you would like to begin afflicted with the zombie curse.",
     )
     c3 = forms.BooleanField(label='C3 Pool',
         required=False,
-        help_text="<div class='reminder'>Check this box if you would like to begin as a member of C3.</div>",
+        help_text="Check this box if you would like to begin as a member of C3.",
     )
     hardcore = forms.BooleanField(label='Legendary Mode',
         required=False,
-        help_text="<div class='reminder'>Check this box if you would like to receive emails about Legendary missions. Legendary missions are available for players who want more plot and story in their game and include challenges that involve critical thinking, difficult side quests, and skills that may not arise in a typical game of HvZ.</div>",
+        help_text="Check this box if you would like to receive emails about Legendary missions. Legendary missions are available for players who want more plot and story in their game and include challenges that involve critical thinking, difficult side quests, and skills that may not arise in a typical game of HvZ.",
     )
     feed = forms.CharField(label='Feed Code',
         min_length=5, max_length=5,
         required=True,
-        help_text="<div class='reminder'>When you are finished entering in all of your other information, have the tabler registering you type in your feed code.<br />TABLERS: Feed codes can only contain the letters A, C, E, L, N, O, P, S, T, W, X, and Z.</div>",
+        help_text="When you are finished entering in all of your other information, have the tabler registering you type in your feed code.<br />TABLERS: Feed codes can only contain the letters A, C, E, L, N, O, P, S, T, W, X, and Z.",
     )
 
 
 class LoginForm(forms.Form):
     email = forms.CharField(label='Email Address',
         required=True,
-        help_text="<div class='reminder'>The email address you used to register for the game.</div>",
+        help_text="The email address you used to register for the game.",
     )
     password = forms.CharField(
         required=True,
         widget=forms.PasswordInput,
-        help_text="<div class='reminder'>Your password for our HvZ site.</div>",
+        help_text="Your password for our HvZ site.",
     )
 
 
@@ -162,17 +162,17 @@ class ThreadForm(forms.Form):
     title = forms.CharField(
         required=True,
         max_length=30,
-        help_text="<div class='reminder'>Type the title of the new thread.</div>",
+        help_text="Type the title of the new thread.",
     )
     description = forms.CharField(
         required=False,
         widget=forms.Textarea,
-        help_text="<div class='reminder'>Type the first post in the new thread.</div>",
+        help_text="Type the first post in the new thread.",
     )
     visibility = forms.ChoiceField(
         required=True,
         choices=TEAMS,
-        help_text="<div class='reminder'>Select who can see and reply to this thread.</div>",
+        help_text="Select who can see and reply to this thread.",
     )
 
 
@@ -187,19 +187,19 @@ class PostForm(forms.Form):
     contents = forms.CharField(
         required=True,
         widget=forms.Textarea,
-        help_text="<div class='reminder'>Type the contents of your reply.</div>",
+        help_text="Type the contents of your reply.",
     )
 
 
 class ResetForm(forms.Form):
     email = forms.EmailField(label='Email Address',
         required=True,
-        help_text="<div class='reminder'>Enter in the email address you used to register for the game. This is the same address that you received a password reset email in.</div>",
+        help_text="Enter in the email address you used to register for the game. This is the same address that you received a password reset email in.",
     )
     password = forms.CharField(
         required=True,
         widget=forms.PasswordInput,
-        help_text="<div class='reminder'>Enter a password that will be easy for you to remember but difficult for others to guess.</div>",
+        help_text="Enter a password that will be easy for you to remember but difficult for others to guess.",
     )
     hash = forms.IntegerField(
         required=True,

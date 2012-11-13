ece82d42fa738479d4c669d69c1fe29d946a464f
Haak Saxberg <haak.erling@gmail.com>
2012-11-13 00:39:59 -0800

    meal model in place

--- HVZ/HVZ/feed/models.py
+++ HVZ/HVZ/feed/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from main.models import Building, Game, Player
 # Create your models here.
+
+
+class Meal(models.Model):
+
+    eater = models.ForeignKey(Player, related_name="meal_set")
+    eaten = models.ForeignKey(Player, related_name="eaten_set")
+
+    game = models.ForeignKey(Game)
+    time = models.DateTimeField()
+    location = models.ForeignKey(Building, null=True, blank=True)
+
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return u"{}: {} -> {}".format()

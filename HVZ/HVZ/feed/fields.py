1f3f683ba2cfd17f0eb235b09b892874f33740bc
Jeff Hemphill <jthemphill@gmail.com>
2013-01-16 19:43:37 -0800

    Fix length validation.
    Apparently fields don't allow a min_length parameter, so I used a custom
    validator.

--- HVZ/HVZ/feed/fields.py
+++ HVZ/HVZ/feed/fields.py
@@ -9,16 +9,21 @@ class FeedCodeField(models.CharField):
     # The characters we allow in a feed code.
     VALID_CHARS = ["A", "C", "E", "L", "K", "N", "P", "Q", "S", "T", "W", "Z"]
 
-    def validate_letters(self):
+    def validate_length(feedcode):
+        """Raise an exception if our length is not exactly FEED_LEN."""
+        if len(feedcode) != FEED_LEN:
+            raise ValidationError(
+                u"The feedcode must be exactly {} characters.".format(FEED_LEN))
+
+    def validate_letters(feedcode):
         """Raise an exception if we contain unapproved characters."""
-        for c in self:
+        for c in feedcode:
             if c not in VALID_CHARS:
                 raise ValidationError(u"{} is not a valid character!".format(c))
 
     def __init__(self, *args, **kwargs):
-        kwargs["min_length"] = self.FEED_LEN
         kwargs["max_length"] = self.FEED_LEN
         super(FeedCodeField, self).__init__(*args, **kwargs)
 
     default_validators = (models.CharField.default_validators +
-                          [validate_letters])
+                          [validate_length, validate_letters])

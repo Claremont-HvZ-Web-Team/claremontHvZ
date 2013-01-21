11d069254ccd293fc0a93f3268f77f5b27557d43
Jeff <jeff@babar.(none)>
2013-01-21 13:43:56 -0800

    Add form-level validation ensuring a user can't reregister for the same game.

--- HVZ/HVZ/main/validators.py
+++ HVZ/HVZ/main/validators.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.core.exceptions import ValidationError
 
+import models
+
 from utils import nearest_game
 
 def feedcode_human(feedcode):
@@ -14,6 +16,11 @@ def validate_chars(feedcode):
     for c in feedcode:
         if c not in settings.VALID_CHARS:
             raise ValidationError(u"{} is not a valid character!".format(c))
+
+def ensure_unregistered(uname):
+    """Ensure the given User has not already registered for this Game."""
+    if models.Player.objects.filter(user__username=uname, game=nearest_game()).exists():
+        raise ValidationError(u"{} has already registered for this Game.".format(uname))
 
 class TimeValidator:
     def __init__(self, game=None):

a9304d8847049436bc9382bbc9458250a7316995
Haak Saxberg <haak.erling@gmail.com>
2013-01-24 23:24:29 -0800

    outbreakbar looks doable, need to choose fonts a little more carefully before continuing probably

--- HVZ/HVZ/main/views.py
+++ HVZ/HVZ/main/views.py
@@ -5,3 +5,10 @@ from django.views.generic import TemplateView
 
 class LandingPage(TemplateView):
     template_name = "main/landing_page.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(LandingPage, self).get_context_data(*args, **kwargs)
+
+        context['is_landing_page'] = True
+
+        return context

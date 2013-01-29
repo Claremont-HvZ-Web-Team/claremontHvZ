0469de81abc8c1a7dc76d8efa336b50570e914ed
Haak Saxberg <haak.erling@gmail.com>
2013-01-28 20:17:07 -0800

    pretty feed eating page, fixes url conf locations

--- HVZ/HVZ/urls.py
+++ HVZ/HVZ/urls.py
@@ -1,9 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-import HVZ.feed.views
-import HVZ.main.views
-
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -15,7 +12,7 @@ urlpatterns = patterns(
     # Coupled to settings.LOGIN_URL for reasons out of my control.
     url(r'^login/', 'django.contrib.auth.views.login', name="login"),
 
-    url(r'^eat/', HVZ.feed.views.EatView.as_view(), name="eat"),
+    url(r'^feed/', include('HVZ.feed.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

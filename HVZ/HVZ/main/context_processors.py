834fdfedbce2da5a3de2ee13e462319ba51f5211
Haak Saxberg <haak.erling@gmail.com>
2013-01-29 00:06:34 -0800

    avoids a ZeroDivisionError, which is a bad thing to not avoid

--- HVZ/HVZ/main/context_processors.py
+++ HVZ/HVZ/main/context_processors.py
@@ -3,5 +3,6 @@ from HVZ.main.models import Player
 
 def inject_outbreak_percentage(request):
     zombies = Player.objects.filter(team='Z').count()
-    percent = (float(zombies) / Player.objects.count()) * 100
+    players = Player.objects.count()
+    percent = (float(zombies) / players) * 100 if players > 0 else 0
     return {'outbreak_percent': min(96, percent)}

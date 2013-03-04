17f89fb6a2b97c94237b9fef00ec8da104329356
Jeff Hemphill <jthemphill@gmail.com>
2013-03-04 13:56:13 -0800

    Minor annoyances.

--- HVZ/HVZ/rules/models.py
+++ HVZ/HVZ/rules/models.py
@@ -1,24 +1,31 @@
 from django.db import models
-from markupfield import fields
+from markupfield import fields as markup
 
 class BaseRule(models.Model):
+    title = models.CharField(max_length=100)
+    body = markup.MarkupField()
+
+    def __unicode__(self):
+        return self.title
+
     class Meta:
         abstract = True
 
-    title = models.CharField(max_length=100)
-    body = fields.MarkupField()
 
 class CoreRule(BaseRule):
     """Rules that are fundamental to the game."""
     pass
 
+
 class LocationRule(BaseRule):
     """Describe the game in different areas."""
     pass
 
+
 class ClassRule(BaseRule):
     """Describe the behavior of superhumans."""
     pass
+
 
 class SpecialInfectedRule(BaseRule):
     """Describe the behavior of superzombies."""

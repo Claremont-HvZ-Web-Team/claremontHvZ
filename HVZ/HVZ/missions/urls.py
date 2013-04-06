8a9a076fb196e66cb5c05790deeaa9257e562ebf
Jeff Hemphill <jthemphill@gmail.com>
2013-04-05 17:52:23 -0700

    Fix typo in mission URL regex.

--- HVZ/HVZ/missions/urls.py
+++ HVZ/HVZ/missions/urls.py
@@ -4,7 +4,7 @@ from HVZ.missions import views
 
 urlpatterns = patterns('HVZ.missions.views',
     url(r'^$', views.PlotListView.as_view(), name="plot_list"),
-    url(r'^(?P<pk>[\d+])/(?P<slug>[-_\w]+)/$',
+    url(r'^(?P<pk>[\d]+)/(?P<slug>[-_\w]+)/$',
         views.PlotDetailView.as_view(),
         name="plot_detail"),
 )

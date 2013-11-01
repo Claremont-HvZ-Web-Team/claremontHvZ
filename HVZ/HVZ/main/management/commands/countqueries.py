9530d5b0f91850ba03e6708d607d7e00107d4642
Jeff Hemphill <jhemphill@fb.com>
2013-11-01 10:06:13 -0700

    Fix countqueries script for incorrect or missing URIs

--- HVZ/HVZ/main/management/commands/countqueries.py
+++ HVZ/HVZ/main/management/commands/countqueries.py
@@ -5,9 +5,9 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         c = Client()
-        path = args[0] or '/players/'
+        path = args[0] if args else '/players/'
 
-        response = c.get(path)
+        response = c.get(path, follow=True)
 
         if response.status_code != 200:
             self.stderr.write('Error %s!' % response.status_code)

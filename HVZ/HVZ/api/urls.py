0be9895e78a4b978d8a7ab9271f6b9d69187571a
Mongoose1021 <rbooth@dhcp-56-20.st.hmc.edu>
2014-03-10 18:36:57 -0700

    Added API for current player's team, a list of all buildings, current player's name, and the human-zombie ratio. This should probably not be on master.

--- HVZ/HVZ/api/urls.py
+++ HVZ/HVZ/api/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, url
 
-from HVZ.api.views import json_get_all_graduation_years
+from HVZ.api.views import json_get_current_player_team
+from HVZ.api.views import json_get_all_buildings
+from HVZ.api.views import json_get_current_player_name
+from HVZ.api.views import json_get_human_zombie_ratio
 
 urlpatterns = patterns('HVZ.api.views',
-    # Replace this view with your own
-    url('^gradyears', json_get_all_graduation_years),
+    url('^team', json_get_current_player_team),
+    url('^buildings', json_get_all_buildings),
+    url('^fullname', json_get_current_player_name),
+    url('^hvzratio', json_get_human_zombie_ratio),
 )
--- HVZ/HVZ/api/views.py
+++ HVZ/HVZ/api/views.py
@@ -3,6 +3,9 @@ import json
 from django.http import HttpResponse
 
 from HVZ.main.models import Player
+from HVZ.main.models import Building
+from django.contrib.auth.models import User
+
 
 def json_get_all_graduation_years(request):
     """A pretty useless function that displays all graduation years.
@@ -13,12 +16,57 @@ def json_get_all_graduation_years(request):
 
     # Check out HVZ/main/models.py for helper functions relating to Players.
     # Player.current_players() returns all Players in the current Game.
-    years = [p.grad_year for p in Player.current_players()]
+    years = Player.logged_in_player(request).team
 
     # json.dumps creates a string from a Python object. You can then
     # read the string and convert it into an Objective-C data
     # structure using NSJSONSerialization in Objective-C.
     json_data = json.dumps(years)
+
+    return HttpResponse(
+        json_data,
+        content_type="application/json"
+    )
+
+
+def json_get_current_player_team(request):
+    """Returns H or Z, corresponding to logged in player's team."""
+
+    team = Player.logged_in_player(request).team
+    json_data = json.dumps(team)
+
+    return HttpResponse(
+        json_data,
+        content_type="application/json"
+    )
+
+def json_get_all_buildings(request):
+    """Returns H or Z, corresponding to logged in player's team."""
+    buildings = [ b.name for b in Building.objects.all() ]
+    json_data = json.dumps(buildings)
+
+    return HttpResponse(
+        json_data,
+        content_type="application/json"
+    )
+
+def json_get_current_player_name(request):
+    """Returns the concatenated first and last names of the logged in player."""
+    name = Player.logged_in_player(request).user.get_full_name();
+    json_data = json.dumps(name);
+
+    return HttpResponse(
+        json_data,
+        content_type="application/json"
+    )
+
+def json_get_human_zombie_ratio(request):
+    """Returns a string describing the spread of
+    the infection, in the format #humans/#zombies"""
+    numHumans = Player.objects.filter(team="H").count()
+    numZombies = Player.objects.filter(team="Z").count()
+    info = str(numHumans) + "/" + str(numZombies)
+    json_data = json.dumps(info)
 
     return HttpResponse(
         json_data,

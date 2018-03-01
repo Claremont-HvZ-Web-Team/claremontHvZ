415e9ac846918a2f07b4c32cfd58098337b17297
Dustin <dkane@hmc.edu>
2018-03-01 09:41:07 -0800

    Put the database location in localsettings

--- hvz/settings.py
+++ hvz/settings.py
@@ -111,13 +111,6 @@ WSGI_APPLICATION = 'hvz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -135,6 +128,8 @@ DATABASES = {
 #         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
 #     },
 # ]
+
+DATABASES = local_settings.DATABASES
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/

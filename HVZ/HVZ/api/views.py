d0759405263460bf0c9e3cbe1954f0fb712fab24
hmc-cs-ctan <ctan@cs.hmc.edu>
2018-03-16 21:26:07 -0700

    Modified web interface for recipients to a drop down menu for all players and also be extract data from the forms. TODO: sender needs authentication in order to send email

--- HVZ/HVZ/api/views.py
+++ HVZ/HVZ/api/views.py
@@ -19,7 +19,7 @@ def json_get_all_emails(request):
     # Check out HVZ/main/models.py for helper functions relating to Players.
     # Player.current_players() returns all Players in the current Game.
     emails = [p.user.email for p in Player.current_players()]
-
+    
     # json.dumps creates a string from a Python object. You can then
     # read the string and convert it into an Objective-C data
     # structure using NSJSONSerialization in Objective-C.
@@ -33,14 +33,28 @@ def json_get_all_emails(request):
 class Mailer(FormView):
     form_class = MailerForm
     template_name = "api/mailer.html"
+    success_url = '/success/'
 
     def dispatch(self, *args, **kwargs):
         return super(Mailer, self).dispatch(*args, **kwargs)
 
     def get_success_url(self):
-        return reverse("success")
+        return super(Mailer, self).get_success_url()
 
     def form_valid(self, form):
-        form.save()
+
+        sender = "mods@claremonthv.org"
+        if form.is_valid():
+            # send email using the self.cleand_data dictionary
+            subject = form.cleaned_data['subject']
+            body = form.cleaned_data['body']
+            recipient_title = form.cleaned_data['recipient']
+
+        if(recipient_title == MailerForm.ALLPLAYERS):
+            recipients = [p.user.email for p in Player.current_players()]
+
+        # TODO: Authentication error for sender
+        send_mail(subject, body, sender, recipients)
+        
         return super(Mailer, self).form_valid(form)
 

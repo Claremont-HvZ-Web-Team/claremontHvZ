8ee0787984bc067f2706513c223646c42d07be88
Adam Schulze <adamschulze@Adams-MacBook-Pro.local>
2018-03-16 22:38:55 -0700

    Added some functions for testing

--- HVZ/HVZ/api/urls.py
+++ HVZ/HVZ/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
-from HVZ.api.views import json_get_all_emails, Mailer
+from HVZ.api.views import json_get_all_emails, Mailer, get_humans
 
 urlpatterns = patterns('HVZ.api.views',
     # Replace this view with your own
     url('^emails', json_get_all_emails),
+
+    url('^humans', get_humans),
 
     url('^mailer', Mailer.as_view())
 )
--- HVZ/HVZ/api/views.py
+++ HVZ/HVZ/api/views.py
@@ -18,7 +18,21 @@ def json_get_all_emails(request):
 
     # Check out HVZ/main/models.py for helper functions relating to Players.
     # Player.current_players() returns all Players in the current Game.
+    
     emails = [p.user.email for p in Player.current_players()]
+    
+    # json.dumps creates a string from a Python object. You can then
+    # read the string and convert it into an Objective-C data
+    # structure using NSJSONSerialization in Objective-C.
+    json_data = json.dumps(emails)
+
+    return HttpResponse(
+        json_data,
+        content_type="application/json"
+    )
+
+def get_humans(request):
+    emails = [p.user.email for p in Player.current_players() if p.team == "H"]
     
     # json.dumps creates a string from a Python object. You can then
     # read the string and convert it into an Objective-C data
@@ -52,6 +66,12 @@ class Mailer(FormView):
 
         if(recipient_title == MailerForm.ALLPLAYERS):
             recipients = [p.user.email for p in Player.current_players()]
+
+        elif(recipient_title == MailerForm.HUMANS):
+            recipients = [p.user.email for p in Player.current_players() if p.team == "H"]
+
+        elif(recipient_title == MailerForm.ZOMBIES):
+            recipients = [p.user.email for p in Player.current_players() if p.team == "Z"]        
 
         # TODO: Authentication error for sender
         send_mail(subject, body, sender, recipients)

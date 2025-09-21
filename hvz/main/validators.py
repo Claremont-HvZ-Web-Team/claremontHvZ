9119d1300eb3be0d2072bdf8d37c29452906774e
Wil Kirkland <wil@gmx.com>
2025-09-20 18:50:42 -0700

    Remove feedcode validator

--- hvz/main/validators.py
+++ hvz/main/validators.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 
 def validate_feedcode_chars(feedcode):
-    """Raise an exception if we contain unapproved characters."""
-    for c in feedcode:
-        if c not in settings.VALID_CHARS:
-            raise ValidationError(u"{} is not a valid character.".format(c))
+    """OLD: Raise an exception if we contain unapproved characters.
+    NEW: All feedcode characters are valid"""
+    # for c in feedcode:
+    #     if c not in settings.VALID_CHARS:
+    #         raise ValidationError(u"{} is not a valid character.".format(c))
+    return
